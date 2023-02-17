f6fdc2f6fe03521beb8c549702a0b26650988b07
BECATRUE <becatrueyjh@gmail.com>
2023-02-17 17:21:27 +0900

    Set up file structure
    According to the description of the issue, add directories and files.
    In frame dir, add base_frame.py for Base classed of framd and logic.
    
    Close: #3

---
+++ frame/base_frame.py
@@ -0,0 +1,14 @@
+"""
+Module of base classes for frame and logic
+"""
+
+class BaseFrame:
+    """
+    Base class of all frames 
+    """
+
+
+class BaseLogic:
+    """
+    Base class of all logics
+    """
