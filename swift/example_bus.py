4ead8d5b87462f965f54bfc3b8ca54c8c241fcb5
BECATRUE <becatrueyjh@gmail.com>
2023-02-19 20:05:40 +0900

    Create example bus module for testing init_bus
    Create example bus module.
    Add ExampleBus class and AnotherExampleBus class to this module.
    
    See also: #7

---
+++ swift/example_bus.py
@@ -0,0 +1,31 @@
+"""Example bus module for testing Swift class.
+"""
+
+class ExampleBus():
+    """Example bus class for testing Swift class.
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def pub1(self):
+        """Public method for meeting Pylint condition.
+        """
+
+    def pub2(self):
+        """Public method for meeting Pylint condition.
+        """
+
+
+class AnotherExampleBus():
+    """Another example bus class for testing Swift class.
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def pub1(self):
+        """Public method for meeting Pylint condition.
+        """
+
+    def pub2(self):
+        """Public method for meeting Pylint condition.
+        """
