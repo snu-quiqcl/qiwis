b234481e72542487535da289105443823f5fb779
Jiyong Kang <kangz12345@gmail.com>
2023-02-20 00:08:38 +0900

    Create Bus class with its signal `received`
    Bus class and its `received` signal is defined.
    Its initializer `__init__()` is also defined by not completed.
    Its attribute `name` is defined.
    
    See also: #6

--- swift/bus.py
+++ swift/bus.py
@@ -1,3 +1,27 @@
 """
 Module for bus features.
 """
+
+from PyQt5.QtCore import QObject, pyqtSignal
+
+class Bus(QObject):
+    """Communication channel for frame logic instances in swift.
+
+    Attributes:
+        name: A string that indicates the name of the bus for identification.
+    
+    Signals:
+        received(str): Emitted when a message is fetched from the queue.
+          Its argument is the fetched message and this signal implies that
+          the message is consumed, i.e., removed from the queue.
+    """
+
+    received = pyqtSignal(str)
+
+    def __init__(self, name: str):
+        """
+        Args:
+            name: Name of the bus for identification.
+        """
+        super().__init__()
+        self.name = name
