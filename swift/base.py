46d9f045b6a797fc47704ab671575ab716d67d18
Jaehun You <becatrueyjh@gmail.com>
2023-02-20 17:05:17 +0900

    Modify base logic class
    Updated to reflect comments.
    
    Close: #11

--- swift/base.py
+++ swift/base.py
@@ -7,25 +7,29 @@ from PyQt5.QtCore import QObject, pyqtSignal
 
 
 class BaseLogic(QObject):
-    """Base Logic class.
-    """
+    """Base Logic class that all Logics should inherit.
 
-    # Signal for broadcasting to a bus
-    # which contains the destination bus name and the message.
-    broadcast = pyqtSignal(str, str)
-    # Signal for receiving a global signal from a bus
-    # which contains the departure bus name and the message.
+    Signals: 
+        broadcastRequested(str, str): A signal for broadcasting to a bus 
+            which contains the destination bus name and the message.
+        received(str, str): A signal for receiving a global signal from a bus
+            which contains the departure bus name and the message.
+    """
+    
+    broadcastRequested = pyqtSignal(str, str)
     received = pyqtSignal(str, str)
 
     def __init__(self, name: str, show: bool, pos: str):
         """Constructor.
 
         Args:
-            name (str): A name of Logic.
-            show (bool): Whether Frames are shown at the beginning.
-            pos (str): An initial position of Frames.
+            name: A string that indicates the name of Logic.
+            show: Whether Frames are shown at the beginning.
+            pos: An initial position of Frames.
+                It should be one of "left", "right", "top", or "bottom"; and is case-insensitive.
+                Otherwise, it will be regarded as default (AllDockWidgetAreas).
         """
-        QObject.__init__(self)
+        super().__init__(self)
         self.name = name
         self.show = show
         self.pos = pos
@@ -36,3 +40,4 @@ class BaseLogic(QObject):
         Returns:
             Iterable: an iterable object of Frame objects for showing
         """
+        return ()
