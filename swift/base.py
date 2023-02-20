6f8b925d69624c6925d185c30ccb96e70f68b69c
Jaehun You <becatrueyjh@gmail.com>
2023-02-20 15:39:30 +0900

    Implement base logic class
    Same as issue #11, implement the below:
    1. broadcast, received signals
    2. name, show, pos fields.
    3. frames() method. (only definition. implementation is for an user.)
    
    See also: #11

--- swift/base.py
+++ swift/base.py
@@ -1,14 +1,38 @@
+"""Base module for Logic.
+
+Every Logic class should be a subclass of BaseLogic.
 """
-Module of base classes for frame and logic
-"""
 
-class BaseFrame:
-    """
-    Base class of all frames 
-    """
+from PyQt5.QtCore import QObject, pyqtSignal
 
 
-class BaseLogic:
+class BaseLogic(QObject):
+    """Base Logic class.
     """
-    Base class of all logics
-    """
+
+    # Signal for broadcasting to a bus
+    # which contains the destination bus name and the message.
+    broadcast = pyqtSignal(str, str)
+    # Signal for receiving a global signal from a bus
+    # which contains the departure bus name and the message.
+    received = pyqtSignal(str, str)
+
+    def __init__(self, name: str, show: bool, pos: str):
+        """Constructor.
+
+        Args:
+            name (str): A name of Logic.
+            show (bool): Whether Frames are shown at the beginning.
+            pos (str): An initial position of Frames.
+        """
+        QObject.__init__(self)
+        self.name = name
+        self.show = show
+        self.pos = pos
+
+    def frames(self):
+        """Get frames for which are managed by the Logic.
+
+        Returns:
+            Iterable: an iterable object of Frame objects for showing
+        """
