51aabea622ab18e3558a08dab54b66ba75f957eb
Jaehun You <becatrueyjh@gmail.com>
2023-02-21 16:04:01 +0900

    Move the function generating number to Logic
    Set a slot of the button in Logic, not Frame.
    
    Close: #16

--- frame/numgen.py
+++ frame/numgen.py
@@ -1,43 +1,66 @@
+"""
+Logic module for generating a random number.
+"""
+
 import sys
 import random
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDockWidget,\
-                            QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt5.QtCore import Qt, pyqtSlot
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QDockWidget,
+                             QWidget, QHBoxLayout, QPushButton, QLabel)
 
 from swift.logic import BaseLogic
 
-class NumGenFrame(QWidget):
+class _NumGenFrame(QWidget):
+    """Frame class for reque
+
+    Args:
+        QWidget (_type_): _description_
+    """
     def __init__(self):
         super().__init__()
         self.init_widget()
 
     def init_widget(self):
+        """Initializes widgets in the frame.
+        """
         self.btn = QPushButton("generate number", self)
-        self.btn.clicked.connect(self.generate_number)
         self.label = QLabel("not generated", self)
 
-        layout = QVBoxLayout(self)
+        layout = QHBoxLayout(self)
         layout.addWidget(self.btn)
         layout.addWidget(self.label)
 
-    def generate_number(self):
-        num = random.randrange(0, 10)
-
-        self.label.clear()
-        self.label.setText(f"generated number: {num}")
-
 
 class NumGenLogic(BaseLogic):
+    """Logic class for managing a frame and generating a random number.
+
+    Attributes:
+        frame: A frame that request generating and show a random number.
+    """
     def __init__(self, name: str):
         super().__init__(name)
-        self.frame = NumGenFrame()
+        self.frame = _NumGenFrame()
+
+        # connect the button clicked signal to the slot generating a number
+        self.frame.btn.clicked.connect(self.generate_number)
 
     def frames(self):
-        return (self.frame,)
+        return (self.frame, )
+
+    @pyqtSlot()
+    def generate_number(self):
+        """Generates and shows a random number when the button is clicked.
+        """
+        num = random.randrange(0, 10)
+
+        self.frame.label.clear()
+        self.frame.label.setText(f"generated number: {num}")
 
 
 def main():
+    """Main function that runs when numgen.py is called. 
+    """
     app = QApplication(sys.argv)
     main_window = QMainWindow()
 
@@ -51,7 +74,7 @@ def main():
 
         main_window.addDockWidget(Qt.LeftDockWidgetArea, dock_widget)
 
-    main_window.showMaximized()
+    main_window.show()
     app.exec_()
 
 
