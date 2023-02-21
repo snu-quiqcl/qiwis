1192be6d84a8efc70596df3bb9c7dadc64a62ab2
Jaehun You <becatrueyjh@gmail.com>
2023-02-21 11:46:01 +0900

    Modify coding style in swift.base module
    Update docstring to start with the singular form of the verb.
    Originally, I planned that show and pos variables to change private,
    but remove them since they are not necessary in base module.
    They will be controlled in swift module.
    
    Close: #14

--- swift/base.py
+++ swift/base.py
@@ -1,10 +1,10 @@
-"""Base module for Logic.
+"""
+Base module for Logic.
 
 Every Logic class should be a subclass of BaseLogic.
 """
 
 from PyQt5.QtCore import QObject, pyqtSignal
-
 
 class BaseLogic(QObject):
     """Base Logic class that all Logics should inherit.
@@ -15,29 +15,23 @@ class BaseLogic(QObject):
         received(str, str): A signal for receiving a global signal from a bus
           which contains the departure bus name and the message.
     """
+
     broadcastRequested = pyqtSignal(str, str)
     received = pyqtSignal(str, str)
 
-    def __init__(self, name: str, show: bool, pos: str):
+    def __init__(self, name: str):
         """Constructor.
 
         Args:
             name: A string that indicates the name of Logic.
-            show: Whether Frames are shown at the beginning.
-            pos: An initial position of Frames.
-              It should be one of "left", "right", "top", or "bottom"; 
-              and is case-insensitive.
-              Otherwise, it will be regarded as default (AllDockWidgetAreas).
         """
         super().__init__()
         self.name = name
-        self.show = show
-        self.pos = pos
 
     def frames(self):
-        """Get frames for which are managed by the Logic.
+        """Gets frames for which are managed by the Logic.
 
         Returns:
-            Iterable: an iterable object of Frame objects for showing
+            Iterable: An iterable object of Frame objects for showing.
         """
         return ()
