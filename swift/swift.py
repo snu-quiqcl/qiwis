187ddf437e5c99ca401c2b0b099e749ea4545bcc
Jaehun You <becatrueyjh@gmail.com>
2023-02-23 20:02:57 +0900

    Modify swift.py
    Move _read_setup_file out of swift class.
    Declare fields in __init__().
    
    See also: #7

--- swift/swift.py
+++ swift/swift.py
@@ -26,39 +26,31 @@ class Swift(QObject):
     """Actual manager for swift system.
 
     Brief procedure:
-        1. Read a set-up file about app and bus.
-        2. Create buses.
-        3. Create apps.
-        4. Show frames of each app.
+        1. Create buses.
+        2. Create apps.
+        3. Show frames of each app.
     """
 
-    def __init__(self, setup_path: str):
+    def __init__(self, setup_env: dict):
         """
         Args:
-            setup_path: A path of set-up file.
+            setup_env: Set-up environment about app and bus.
         """
         super().__init__()
-        self._read_setup_file(setup_path)
-        self._init_bus()
-        self._init_app()
-        self._show_frame()
-
-    def _read_setup_file(self, setup_path: str):
-        """Reads set-up information about app and bus from set-up file.
-
-        Args:
-            setup_path: A path of set-up file.
-        """
-        with open(setup_path, encoding="utf-8") as setup_file:
-            setup_data = json.load(setup_file)
-            self._setup_app = setup_data["app"]
-            self._setup_bus = setup_data["bus"]
-
-    def _init_bus(self):
-        """Initializes global buses using set-up environment."""
         self._buses = {}
-        self._subs = {}
-        for name, info in self._setup_bus.items():
+        self._apps = {}
+        self._subscribers = {}
+        self._init_bus(setup_env["bus"])
+        self._init_app(setup_env["app"])
+        self._show_frame(setup_env["app"])
+
+    def _init_bus(self, setup_bus: dict):
+        """Initializes global buses using set-up environment.
+        
+        Args:
+            setup_bus: Set-up environment about bus.
+        """
+        for name, info in setup_bus.items():
             # create a bus
             if "timeout" in info:
                 timeout = info["timeout"]
@@ -70,12 +62,15 @@ class Swift(QObject):
             bus.start()
             # store the bus
             self._buses[name] = bus
-            self._subs[name] = []
+            self._subscribers[name] = []
 
-    def _init_app(self):
-        """Initializes apps using set-up environment."""
-        self._apps = {}
-        for name, info in self._setup_app.items():
+    def _init_app(self, setup_app: dict):
+        """Initializes apps using set-up environment.
+        
+        Args:
+            setup_app: Set-up environment about app.
+        """
+        for name, info in setup_app.items():
             # import the app module
             path = info.get("path", ".")
             mod_name, cls_name = info["module"], info["class"]
@@ -88,12 +83,16 @@ class Swift(QObject):
             app.broadcastRequested.connect(self._route_to_bus)
             # add the app to the list of subscribers on each bus
             for bus_name in info["bus"]:
-                self._subs[bus_name].append(app)
+                self._subscribers[bus_name].append(app)
             # store the app
             self._apps[name] = app
 
-    def _show_frame(self):
-        """Shows frames of each app."""
+    def _show_frame(self, setup_app: dict):
+        """Shows frames of each app.
+        
+        Args:
+            setup_app: Set-up environment about app.
+        """
         self._mainWindow = QMainWindow()
         pos_to_area = {
             "left": Qt.LeftDockWidgetArea,
@@ -101,7 +100,7 @@ class Swift(QObject):
             "top": Qt.TopDockWidgetArea,
             "bottom": Qt.BottomDockWidgetArea
         }
-        for name, info in self._setup_app.items():
+        for name, info in setup_app.items():
             # show frames if the 'show' option is true.
             if info["show"]:
                 frames = self._apps[name].frames()
@@ -136,7 +135,7 @@ class Swift(QObject):
         """
         bus_name = self.sender().name
         # Emit a signal of all apps that subscribe to the bus.
-        for app in self._subs[bus_name]:
+        for app in self._subscribers[bus_name]:
             app.received.emit(bus_name, msg)
 
 
@@ -154,7 +153,7 @@ def _add_to_path(path):
         sys.modules = old_modules
 
 
-def get_argparser():
+def _get_argparser():
     """Parses command line arguments
 
     -s, --setup: A path of set-up file.
@@ -172,11 +171,27 @@ def get_argparser():
     return parser
 
 
+def _read_setup_file(setup_path: str):
+    """Reads set-up information about app and bus from set-up file.
+
+    Args:
+        setup_path: A path of set-up file.
+    """
+    with open(setup_path, encoding="utf-8") as setup_file:
+        setup_data = json.load(setup_file)
+        setup_app = setup_data["app"]
+        setup_bus = setup_data["bus"]
+    return {"app": setup_app, "bus": setup_bus}
+
+
 def main():
     """Main function that runs when swift.py is called."""
-    args = get_argparser().parse_args()
+    args = _get_argparser().parse_args()
+    # read set-up information
+    setup_env = _read_setup_file(args.setup_path)
+
     app = QApplication(sys.argv)
-    _swift = Swift(args.setup_path)
+    _swift = Swift(setup_env)
     app.exec_()
 
 
