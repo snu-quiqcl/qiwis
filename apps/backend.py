dd27e527184a4a8f357c57d1805acfcffb638d6c
Jaehun You <becatrueyjh@gmail.com>
2023-02-24 14:57:48 +0900

    Implement save function
    See also: #20

--- apps/backend.py
+++ apps/backend.py
@@ -11,3 +11,19 @@ def generate():
         int: Generated number.
     """
     return random.randrange(0, 100)
+
+
+def save(_num: int, db_name: str):
+    """Store the number in the database.
+
+    Args:
+        num: Number to store
+        db_name: A string that indicates the name of database
+
+    Returns:
+        bool: True if store succeeds, otherwise False.
+    """
+    if db_name is None:
+        return False
+
+    return True
