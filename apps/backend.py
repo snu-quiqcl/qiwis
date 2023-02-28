3a2ad48df6b0cbac5e791261aeccde068dccf391
Jaehun You <becatrueyjh@gmail.com>
2023-02-28 18:38:45 +0900

    Implement write function and modify numgen
    See also: #24

--- apps/backend.py
+++ apps/backend.py
@@ -3,6 +3,8 @@ Backend module for offering various functions.
 """
 
 import random
+import sqlite3
+from datetime import datetime
 
 def generate() -> int:
     """Generates a random number from 0 to 99.
@@ -13,18 +15,25 @@ def generate() -> int:
     return random.randrange(0, 100)
 
 
-# TODO(BECATRUE): Change _num to num when saving to a database is implemented.
-def save(_num: int, db_name: str) -> bool:
-    """Store the number in the database.
+def write(db_path: str, table: str, value) -> bool:
+    """Write the value in the database.
 
     Args:
-        _num: Number to store.
-        db_name: A string that indicates the name of database.
+        db_path: A path of database file.
+        table: A name of table to write down.
+        value: A value to write to the given location.
 
     Returns:
-        True if store succeeds, otherwise False.
+        True if writing is successful, otherwise False.
     """
-    if db_name == "None":
+    if db_path == "None":  # if there is no database
         return False
-
+    con = sqlite3.connect(db_path)
+    try:
+        with con:
+            con.execute(f"insert into {table} values (?, ?)", (value, str(datetime.now())))
+    except sqlite3.Error as e:
+        print(e)
+        return False
+    con.close()
     return True
