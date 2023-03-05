cb0b0e87acdc6db4e6ee26c6c86406f209e692f3
Jaehun You <becatrueyjh@gmail.com>
2023-03-05 17:19:35 +0900

    Implement all widgets of ViewerFrame
    Add dbBoxes, calculateButton, and numberLable.
    
    See also: #28

--- apps/datacalc.py
+++ apps/datacalc.py
@@ -9,12 +9,33 @@ from swift.app import BaseApp
 from apps.backend import read
 
 class ViewerFrame(QWidget):
+    """Frame of for selecting databases and showing the calculated number.
+    
+    Attributes:
+        dbBoxes: A dictionary containing two comboboxes for selecting the database 
+          from which the value of A and B is fetched.
+        calculateButton: A button for calculating the sum of recently fetched 'A' and 'B'.
+        numberLabel: A label showing the sum, or an error message if something goes wrong.
+    """
     def __init__(self, parent=None):
         """
         Args:
             parent: A parent widget.
         """
         super().__init__(parent=parent)
+        # widgets
+        self.dbBoxes = {
+            "A": QComboBox(self),
+            "B": QComboBox(self),
+        }
+        self.calculateButton = QPushButton("sum", self)
+        self.numberLabel = QLabel("not calculated", self)
+        # layout
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.dbBoxes["A"])
+        layout.addWidget(self.dbBoxes["B"])
+        layout.addWidget(self.calculateButton)
+        layout.addWidget(self.numberLabel)
 
 
 class DataCalcApp(BaseApp):
