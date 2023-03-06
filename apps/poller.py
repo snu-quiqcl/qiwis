7d4a8d45f29e695e2665b9b6bec8345c0e707f09
Jaehun You <becatrueyjh@gmail.com>
2023-03-06 16:56:10 +0900

    Implement slot of received
    Implement updating database list.
    
    See also: #37

--- apps/poller.py
+++ apps/poller.py
@@ -2,6 +2,9 @@
 App module for polling a number and saving it into the selected database.
 """
 
+import json
+
+from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox, QSpinBox, QLabel
 
 from swift.app import BaseApp
@@ -38,10 +41,18 @@ class ViewerFrame(QWidget):
 
 
 class PollerApp(BaseApp):
-    def __init__(self, name: str, table: str):
+    def __init__(self, name: str, table: str = "B"):
+        """
+        Args:
+            table: A name of table to store the polled number.
+        """
         super().__init__(name)
         self.table = table
+        self.dbs = {"": ""}
+        self.dbName = ""
         self.viewerFrame = ViewerFrame()
+        # connect signals to slots
+        self.received.connect(self.updateDB)
 
     def frames(self):
         """Gets frames for which are managed by the app.
@@ -50,3 +61,38 @@ class PollerApp(BaseApp):
             A tuple containing frames for showing.
         """
         return (self.viewerFrame,)
+
+    @pyqtSlot(str, str)
+    def updateDB(self, busName: str, msg: str):
+        """Updates the database list using the transferred message.
+
+        This is a slot for received signal.
+
+        Args:
+            busName: A name of the bus that transfered the signal.
+            msg: An input message to be transferred through the bus.
+              The structure follows the message protocol of DBMgrApp.
+        """
+        if busName == "dbbus":
+            try:
+                msg = json.loads(msg)
+            except json.JSONDecodeError as e:
+                print(f"apps.numgen.updateDB(): {e!r}")
+            else:
+                orgDbName = self.dbName
+                self.dbs = {"": ""}
+                self.viewerFrame.dbBox.clear()
+                self.viewerFrame.dbBox.addItem("")
+                for db in msg.get("db", ()):
+                    if all(key in db for key in ("name", "path")):
+                        name, path = db["name"], db["path"]
+                        self.dbs[name] = path
+                        self.viewerFrame.dbBox.addItem(name)
+                    else:
+                        print(f"The message was ignored because "
+                              f"the database {db} has no such key; name or path.")
+                if orgDbName in self.dbs:
+                    self.viewerFrame.dbBox.setCurrentText(orgDbName)
+        else:
+            print(f"The message was ignored because "
+                  f"the treatment for the bus {busName} is not implemented.")
