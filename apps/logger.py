73adcd8d8e44c7be79d379901f2abd8186bf3758
Ahrrri <c11sh0117@gmail.com>
2023-03-07 13:17:25 +0900

    Update confirmation frame

--- apps/logger.py
+++ apps/logger.py
@@ -2,11 +2,10 @@
 App module for logging.
 """
 
-import sys
+import time
 
-from PyQt5.QtCore import Qt, pyqtSlot
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QDockWidget, QWidget,
-                             QVBoxLayout, QPushButton, QTextEdit)
+from PyQt5.QtCore import pyqtSlot, pyqtSignal
+from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QPushButton, QTextEdit, QLabel)
 
 from swift.app import BaseApp
 
@@ -14,17 +13,55 @@ class LoggerFrame(QWidget):
     """Frame for logging.
     
     Attributes:
-        textEdit: A textEdit which shows all logs.
+        logEdit: A textEdit which shows all logs.
         clearButton: A button for clearing all logs.
     """
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         self.logEdit = QTextEdit(self)
-        self.clearButton = QPushButton(self)
+        self.logEdit.setReadOnly(True)
+        self.clearButton = QPushButton("Clear")
 
         layout = QVBoxLayout(self)
         layout.addWidget(self.logEdit)
         layout.addWidget(self.clearButton)
+
+
+class ConfirmClearingFrame(QWidget):
+    """
+    A confirmation frame for log clearing.
+    """
+    confirmed = pyqtSignal()
+
+    def __init__(self):
+        """
+        Initializes confirmation frame
+        """
+        super().__init__()
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.label = QLabel("Are you sure to clear?")
+        self.buttonOK = QPushButton("OK")
+        self.buttonCancel = QPushButton("Cancel")
+        layout.addWidget(self.label)
+        layout.addWidget(self.buttonOK)
+        layout.addWidget(self.buttonCancel)
+
+        self.buttonOK.clicked.connect(self.buttonOKClicked)
+        self.buttonCancel.clicked.connect(self.buttonCancelClicked)
+
+    def buttonOKClicked(self):
+        """Clicks OK to clear log.
+        """
+        self.confirmed.emit()
+        self.close()
+
+    def buttonCancelClicked(self):
+        """Clicks Cancel not to clear log
+        """
+        self.close()
+
 
 class LoggerApp(BaseApp):
     """App for logging.
@@ -37,9 +74,12 @@ class LoggerApp(BaseApp):
     def __init__(self, name: str):
         super().__init__(name)
         self.loggerFrame = LoggerFrame()
+
         # connect signals to slots
         self.received.connect(self.addLog)
-        self.loggerFrame.clearButton.clicked.connect(self.clearLog)
+        self.loggerFrame.clearButton.clicked.connect(self.checkToClear)
+        self.confirmFrame = ConfirmClearingFrame()
+        self.confirmFrame.confirmed.connect(self.clearLog)
 
     def frames(self):
         """Gets frames for which are managed by the app.
@@ -47,32 +87,27 @@ class LoggerApp(BaseApp):
         Returns:
             A tuple containing frames for showing.
         """
-        return (self.loggerFrame, )
+        return (self.loggerFrame,)
 
     @pyqtSlot(str, str)
-    def addLog(self, bus_name, msg):
-        """Add a log message and bus name"""
-        self.loggerFrame.logEdit.insertPlainText(f"[{bus_name}]: {msg}")
+    def addLog(self, busName, msg):
+        """Adds a bus name and log message.
+
+        Args:
+            busName (str): the name of bus
+            msg (str): log message
+        """
+        self.loggerFrame.logEdit.insertPlainText(
+            f"{time.strftime('%c', time.localtime(time.time()))}[{busName}]: {msg}\n")
+
+    @pyqtSlot()
+    def checkToClear(self):
+        """Show a confirmation frame for log clearing.
+        """
+        self.confirmFrame.show()
 
     @pyqtSlot()
     def clearLog(self):
-        """Clear the log text edit"""
+        """Clear the log text edit
+        """
         self.loggerFrame.logEdit.clear()
-
-def main():
-    """Main function that runs when numgen module is executed rather than imported."""
-    _app = QApplication(sys.argv)
-    mainWindow = QMainWindow()
-    # create an app
-    app = LoggerApp("logger")
-    # get frames from the app and add them as dock widgets
-    for frame in app.frames():
-        dockWidget = QDockWidget("logger", mainWindow)
-        dockWidget.setWidget(frame)
-        mainWindow.addDockWidget(Qt.LeftDockWidgetArea, dockWidget)
-    mainWindow.show()
-    _app.exec_()
-
-
-if __name__ == "__main__":
-    main()
