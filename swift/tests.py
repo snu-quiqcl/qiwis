5a4c8196b2fde8925a24b413b03c96a61c5fe35e
Jaehun You <becatrueyjh@gmail.com>
2023-03-16 14:54:48 +0900

    Add docstring
    Close: #88

--- swift/tests.py
+++ swift/tests.py
@@ -1,13 +1,21 @@
+"""
+Module for testing swift package.
+"""
+
 import unittest
 from collections.abc import Iterable
 
 from swift.app import BaseApp
 
 class AppTest(unittest.TestCase):
+    """Unit test for app.py."""
+
     def setUp(self):
+        """Create an app every time."""
         self.app = BaseApp("name")
 
     def test_frames(self):
+        """Test BaseApp.frames()."""
         self.assertIsInstance(self.app.frames(), Iterable)
 
 
