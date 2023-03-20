cffe48171dd566f08b6002e583fd5fa2eb27b474
Jaehun You <becatrueyjh@gmail.com>
2023-03-20 16:20:32 +0900

    Test BaseApp class of swift.app
    See also: #88

--- tests/test_swift.py
+++ tests/test_swift.py
@@ -5,7 +5,10 @@ Module for testing swift package.
 import unittest
 from collections.abc import Iterable
 
+from PyQt5.QtCore import QObject
+
 from swift.app import BaseApp
+
 
 class AppTest(unittest.TestCase):
     """Unit test for app.py."""
@@ -13,6 +16,14 @@ class AppTest(unittest.TestCase):
     def setUp(self):
         """Create an app every time."""
         self.app = BaseApp("name")
+
+    def test_init(self):
+        """Test if fields are initialized as constructor arguments."""
+        self.assertEqual(self.app.name, "name")
+
+    def test_set_parent(self):
+        """Test if a parent constructor argument can be set as a QObject."""
+        BaseApp("name", QObject())
 
     def test_frames(self):
         """Test BaseApp.frames()."""
