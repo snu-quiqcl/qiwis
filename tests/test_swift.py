9f99fe4e61e650c433d3bb1fc39cd276a7a027a0
Jaehun You <becatrueyjh@gmail.com>
2023-03-21 16:23:17 +0900

    Initialize testing for swift
    Define mock appInfos and busInfos.
    Check if swift is created well.
    
    See also: #107

--- tests/test_swift.py
+++ tests/test_swift.py
@@ -3,11 +3,14 @@ Module for testing swift package.
 """
 
 import unittest
+import sys
 from collections.abc import Iterable
 
 from PyQt5.QtCore import QObject
+from PyQt5.QtWidgets import QApplication
 
 from swift.app import BaseApp
+from swift.swift import Swift, AppInfo, BusInfo
 
 class AppTest(unittest.TestCase):
     """Unit test for app.py."""
@@ -27,6 +30,41 @@ class AppTest(unittest.TestCase):
     def test_frames(self):
         """Test BaseApp.frames()."""
         self.assertIsInstance(self.app.frames(), Iterable)
+
+
+class SwiftTest(unittest.TestCase):
+    """Unit test for swift.py"""
+
+    appInfos = {
+        "app1": AppInfo(
+            module="module1",
+            cls="cls1",
+            path="path1",
+            show="false",
+            pos="left",
+            bus=["bus1", "bus2"],
+            args={"arg1": "value1"}
+        ),
+        "app2": AppInfo(
+            module="module2",
+            cls="cls2"
+        )
+    }
+
+    busInfos = {
+        "bus1": BusInfo(
+            timeout=5.0
+        ),
+        "bus2": BusInfo()
+    }
+
+    def setUp(self):
+        """Create a QApplication and Swift object every time."""
+        self.qapp = QApplication(sys.argv)
+        self.swift = Swift(SwiftTest.appInfos, SwiftTest.busInfos)
+
+    def test_init(self):
+        pass
 
 
 if __name__ == "__main__":
