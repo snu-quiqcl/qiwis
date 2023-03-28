8e58826e714a029802589e50ce120a79a07f79f6
BECATRUE <becatrueyjh@gmail.com>
2023-03-28 18:42:03 +0900

    Import only modules
    see also: #107

--- tests/test_swift.py
+++ tests/test_swift.py
@@ -12,14 +12,10 @@ from collections.abc import Iterable
 from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel
 
-from swift.app import BaseApp
-from swift.swift import (
-    Swift, AppInfo, parse, strinfo,
-    _add_to_path, _get_argparser, _read_setup_file, main
-)
+from swift import app, swift
 
 APP_INFOS = {
-    "app1": AppInfo(
+    "app1": swift.AppInfo(
         module="module1",
         cls="cls1",
         path="path1",
@@ -28,7 +24,7 @@ APP_INFOS = {
         bus=["bus1", "bus2"],
         args={"arg1": "value1"}
     ),
-    "app2": AppInfo(
+    "app2": swift.AppInfo(
         module="module2",
         cls="cls2"
     )
@@ -63,7 +59,7 @@ class AppTest(unittest.TestCase):
 
     def setUp(self):
         """Create an app every time."""
-        self.app = BaseApp("name")
+        self.app = app.BaseApp("name")
 
     def test_init(self):
         """Test if fields are initialized as constructor arguments."""
@@ -71,7 +67,7 @@ class AppTest(unittest.TestCase):
 
     def test_set_parent(self):
         """Test if a parent constructor argument can be set as a QObject."""
-        BaseApp("name", QObject())
+        app.BaseApp("name", QObject())
 
     def test_frames(self):
         """Test BaseApp.frames()."""
@@ -93,7 +89,7 @@ class SwiftTest(unittest.TestCase):
         self.buses = set()
         for appInfo in APP_INFOS.values():
             self.buses.update(appInfo.bus)
-        self.swift = Swift(APP_INFOS)
+        self.swift = swift.Swift(APP_INFOS)
 
     def test_init(self):
         """Test if swift is initialized correctly."""
@@ -123,25 +119,25 @@ class SwiftFunctionTest(unittest.TestCase):
 
     def test_parse(self):
         """Test parse()."""
-        self.assertEqual(parse(AppInfo, APP_JSONS["app1"]), APP_INFOS["app1"])
-        self.assertEqual(parse(AppInfo, APP_JSONS["app2_default"]), APP_INFOS["app2"])
+        self.assertEqual(swift.parse(swift.AppInfo, APP_JSONS["app1"]), APP_INFOS["app1"])
+        self.assertEqual(swift.parse(swift.AppInfo, APP_JSONS["app2_default"]), APP_INFOS["app2"])
 
     def test_strinfo(self):
         """Test strinfo()."""
-        self.assertEqual(strinfo(APP_INFOS["app1"]), APP_JSONS["app1"])
-        self.assertEqual(strinfo(APP_INFOS["app2"]), APP_JSONS["app2"])
+        self.assertEqual(swift.strinfo(APP_INFOS["app1"]), APP_JSONS["app1"])
+        self.assertEqual(swift.strinfo(APP_INFOS["app2"]), APP_JSONS["app2"])
 
     def test_add_to_path(self):
         """Test _add_to_path()."""
         old_path = sys.path.copy()
-        with _add_to_path(os.path.dirname("test_dir")):
+        with swift._add_to_path(os.path.dirname("test_dir")):
             self.assertNotEqual(old_path, sys.path)
         self.assertEqual(old_path, sys.path)
 
     @patch.object(sys, "argv", ["", "-s", "test_setup.json"])
     def test_get_argparser(self):
         """Test _get_argparser()."""
-        parser = _get_argparser()
+        parser = swift._get_argparser()
         args = parser.parse_args()
         self.assertEqual(parser.description, "SNU widget integration framework for PyQt")
         self.assertEqual(args.setup_path, "test_setup.json")
@@ -149,14 +145,14 @@ class SwiftFunctionTest(unittest.TestCase):
     @patch.object(sys, "argv", [""])
     def test_get_argparser_default(self):
         """Test _get_argparser() with default options."""
-        args = _get_argparser().parse_args()
+        args = swift._get_argparser().parse_args()
         self.assertEqual(args.setup_path, "./setup.json")
 
     @patch("builtins.open")
     @patch("json.load", return_value={"app": APP_DICTS})
     def test_read_setup_file(self, mock_open, mock_load):
         """Test _read_setup_file()."""
-        self.assertEqual(_read_setup_file(""), APP_INFOS)
+        self.assertEqual(swift._read_setup_file(""), APP_INFOS)
         mock_open.assert_called_once()
         mock_load.assert_called_once()
 
@@ -166,7 +162,7 @@ class SwiftFunctionTest(unittest.TestCase):
     @patch("PyQt5.QtWidgets.QApplication.exec_")
     def test_main(self, mock_get_argparser, mock_read_setup_file, mock_swift, mock_exec_):
         """Test main()."""
-        main()
+        swift.main()
         mock_get_argparser.assert_called_once()
         mock_read_setup_file.assert_called_once()
         mock_swift.assert_called_once()
