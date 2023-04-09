e46e1488b31bdc8d52f658c42e2459669fd5e990
Jiyong Kang <kangz12345@gmail.com>
2023-04-09 22:36:15 +0900

    Add an argument to `swiftcallReturned`
    This is a requested message, which will be useful
    to distinguish when an app requests multiple swiftcalls.
    
    See also: #131

--- swift/app.py
+++ swift/app.py
@@ -25,7 +25,7 @@ class BaseApp(QObject):
     broadcastRequested = pyqtSignal(str, str)
     received = pyqtSignal(str, str)
     swiftcallRequested = pyqtSignal(str)
-    swiftcallReturned = pyqtSignal(str)
+    swiftcallReturned = pyqtSignal(str, str)
 
     def __init__(self, name: str, parent: Optional[QObject] = None):
         """
