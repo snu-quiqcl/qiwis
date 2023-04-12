677f269e79deae21f7b0318914217d69afa4058b
Jiyong Kang <kangz12345@gmail.com>
2023-04-13 00:41:11 +0900

    Break lines in TODO comments
    Resolved pylint warnings.

--- swift/swift.py
+++ swift/swift.py
@@ -69,7 +69,8 @@ class AppInfo(Serializable):
     args: Optional[Mapping[str, Any]] = None
 
 
-# TODO(kangz12345): Make this and strinfo() a pair. Change this to loads() whenever we can rename this symbol.
+# TODO(kangz12345): Make this and strinfo() a pair.
+# Change this to loads() whenever we can rename this symbol.
 def parse(cls: Type[T], kwargs: str) -> T:
     """Returns a new cls instance from a JSON string.
 
@@ -86,7 +87,8 @@ def parse(cls: Type[T], kwargs: str) -> T:
     return cls(**json.loads(kwargs))
 
 
-# TODO(kangz12345): Method name looks too specific. Change this to dumps() whenever we can rename this symbol.
+# TODO(kangz12345): Method name looks too specific.
+# Change this to dumps() whenever we can rename this symbol.
 def strinfo(info: Serializable) -> str:
     """Returns a JSON string converted from the given info.
 
