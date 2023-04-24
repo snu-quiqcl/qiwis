bdc26ca67b88be61ccb14625a5ed221df11c6ddc
Jiyong Kang <kangz12345@gmail.com>
2023-04-24 23:36:10 +0900

    Implement `channelNames()`
    See also: #151

--- swift/swift.py
+++ swift/swift.py
@@ -212,6 +212,10 @@ class Swift(QObject):
         """Returns the names of the apps including whose frames are hidden."""
         return self._apps.keys()
 
+    def channelNames(self) -> KeysView[str]:
+        """Returns the names of the channels."""
+        return self._subscribers.keys()
+
     def createApp(self, name: str, info: AppInfo):
         """Creates an app and shows their frames using set-up environment.
         
