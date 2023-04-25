c2e7577cd3a60311e8c714386cfe130bc6d60ad2
BECATRUE <becatrueyjh@gmail.com>
2023-04-25 12:30:51 +0900

    Add console script
    See also: #145

--- setup.py
+++ setup.py
@@ -18,7 +18,10 @@ setup(
     long_description="A framework for integration of PyQt widgets where they can communicate with each other. "
                      "This project is mainly developed for trapped ion experiment controller GUI in SNU QuIQCL.",
     download_url="https://github.com/snu-quiqcl/swift/releases/tag/v2.0.0",  # indicate the url of the following release
-    license="MIT license"
-    install_requires=["pyqt5"]
+    license="MIT license",
+    install_requires=["pyqt5"],
     packages=find_packages(include=["swift", "swift.*"]),
+    entry_points={
+        "console_scripts": ["swift = swift.swift:main"]
+    }
 )
