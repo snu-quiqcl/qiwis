e49b6a03ed6354180f01525eeed69618e923c2fa
BECATRUE <becatrueyjh@gmail.com>
2023-04-25 12:21:59 +0900

    Add author info and url
    See also: #145

--- setup.py
+++ setup.py
@@ -9,10 +9,13 @@ if sys.version_info[:2] < (3, 7):
     raise Exception("You need Python 3.7+")
 
 setup(
-    name='swift',
-    version='1.0.0',
-    packages=find_packages(include=['swift', 'swift.*']),
+    name="swift",
+    version="2.0.0",  # indicate the following version
+    author="QuIQCL",
+    author_email="kangz12345@snu.ac.kr",
+    url="https://github.com/snu-quiqcl/swift",
+    packages=find_packages(include=["swift", "swift.*"]),
     install_requires=[
-        'pyqt5'
+        "pyqt5"
     ]
 )
