a0ea24b2329b4d20d0aa97829ec7ba1f6b0a0716
BECATRUE <becatrueyjh@gmail.com>
2023-04-25 12:43:04 +0900

    Modify pylint warning
    See also: #145

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 Set-up file for releasing this package.
 """
 
-from setuptools import setup, find_packages
 import sys
+from setuptools import setup, find_packages
 
 if sys.version_info[:2] < (3, 7):
-    raise Exception("You need Python 3.7+")
+    raise Exception("You need Python 3.7+")  # pylint: disable=broad-except
 
 setup(
     name="swift",
@@ -15,9 +15,10 @@ setup(
     author_email="kangz12345@snu.ac.kr",
     url="https://github.com/snu-quiqcl/swift",
     description="SNU widget integration framework for PyQt",
-    long_description="A framework for integration of PyQt widgets where they can communicate with each other. "
-                     "This project is mainly developed for trapped ion experiment controller GUI in SNU QuIQCL.",
-    download_url="https://github.com/snu-quiqcl/swift/releases/tag/v2.0.0",  # indicate the url of the following release
+    long_description=
+        "A framework for integration of PyQt widgets where they can communicate with each other. "
+        "This project is mainly developed for trapped ion experiment controller GUI in SNU QuIQCL.",
+    download_url="https://github.com/snu-quiqcl/swift/releases/tag/v2.0.0",
     license="MIT license",
     install_requires=["pyqt5"],
     packages=find_packages(include=["swift", "swift.*"]),
