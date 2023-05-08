58bc98ef2c8d9bcfeac6a2b7159d188429f835b7
BECATRUE <becatrueyjh@gmail.com>
2023-05-09 07:43:20 +0900

    Replace in poller example
    See also: #156

--- examples/poller.py
+++ examples/poller.py
@@ -8,7 +8,7 @@ from typing import Any, Optional, Tuple
 from PyQt5.QtCore import QObject, pyqtSlot, QTimer
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QComboBox, QSpinBox, QLabel
 
-from swift import BaseApp
+from qiwi import BaseApp
 from examples.backend import poll, write
 
 class ViewerFrame(QWidget):
