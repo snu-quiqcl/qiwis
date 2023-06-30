a259ec8c47d2ef03d318a345729e28504937f90e
Aijuh <aijuh1229@snu.ac.kr>
2023-06-30 11:01:42 +0900

    Update logger.py

--- examples/logger.py
+++ examples/logger.py
@@ -4,7 +4,7 @@ App module for logging.
 
 import time
 import logging
-from typing import Any, Optional, Tuple
+from typing import Any, Optional, Tuple 
 from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QTextEdit, QLabel, QDialogButtonBox
 from qiwis import BaseApp
@@ -19,18 +19,17 @@ class LoggingHandler(QObject, logging.Handler):
 
     Sends a log message to connected function using emit.
     """
-
     def __init__(self, slotfunc):
         """Connect an input function to the signal.
 
         Args:
-            slotfunc: function connected to signal
+            slotfunc: class method function connected to signal
         """
         super().__init__()
         self.signaller = Signaller()
         self.signaller.signal.connect(slotfunc)
 
-    def emit(self, record):
+    def emit(self, record: logging.LogRecord):
         """ Emits input signal to connected function."""
         s = self.format(record)
         self.signaller.signal.emit(s)
